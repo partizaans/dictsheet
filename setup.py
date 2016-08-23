af1415d43306987f667f766133b7c656d3afd7d2
anqing.huang <anqing.huang@yulore.com>
2016-08-23 17:09:02 +0800

    [Feature] Add essential file for pypi testing.

--- setup.py
+++ setup.py
@@ -1,15 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = 'dispread',
+    name = 'dictsheet',
     version = '0.0.1',
-    keywords = ('dispread'),
-    description = 'This is dispread',
+    keywords = ('dictsheet', 'spreadsheet', 'gspread'),
+    description = 'Dict wrapper for google spreadsheet',
     license = 'MIT License',
-    install_requires = ['gspread==0.4.1', 'httplib2==0.9.2', 
-                        'oauth2client==2.2.0','pprintpp==0.2.3',
-                        'py==1.4.31','pyasn1==0.1.9','pyasn1-modules==0.0.8',
-                        'pytest==2.9.2','requests==2.10.0','rsa==3.4.2','six==1.10.0'],
+    install_requires = ['gspread==0.4.1'],
+    url = 'https://github.com/previa/dictsheet',
 
     author = 'Chandler Huang, Xander Li',
     author_email = 'previa@gmail.com',

cb389e68b6bbc7e80d816b9e09540f2ecbbc2bfe
anqing.huang <anqing.huang@yulore.com>
2016-08-23 22:39:20 +0800

    [Feature] Update version info.

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'dictsheet',
-    version = '0.0.1',
+    version = '0.0.3',
     keywords = ('dictsheet', 'spreadsheet', 'gspread'),
     description = 'Dict wrapper for google spreadsheet',
     license = 'MIT License',
-    install_requires = ['gspread==0.4.1'],
+    install_requires = ['gspread>=0.4.1'],
     url = 'https://github.com/previa/dictsheet',
 
     author = 'Chandler Huang, Xander Li',

8c44e73d6b952b4586f117e96847f4f8bf08eba0
anqing.huang <anqing.huang@yulore.com>
2016-08-25 12:02:20 +0800

    [Feature] Use README.md as long_description.

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 from setuptools import setup, find_packages
 from pip.req import parse_requirements
 
+with open("README.md") as file:
+    long_description = file.read()
+
+
 install_reqs = parse_requirements('requirements.txt', session=False)
 reqs = [str(ir.req) for ir in install_reqs]
 # REFERENCE:
@@ -15,10 +19,22 @@ setup(
     install_requires = reqs,
     data_files = ['requirements.txt', 'README.md', 'LICENSE.txt'],
     url = 'https://github.com/previa/dictsheet',
-
+    long_description = long_description,
     author = 'Chandler Huang, Xander Li',
     author_email = 'previa@gmail.com',
     
     packages = find_packages(),
     platforms = 'any',
+    classifiers = [
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 2.8",
+        "Programming Language :: Python :: 2.9",
+        "Programming Language :: Python :: 3.0",
+        "Programming Language :: Python :: 3.1",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+    ],
 )
